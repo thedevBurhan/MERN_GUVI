70eacfaa67a6ea8727aae016f7ec0be3bab51b9d
thedevBurhan <smdburhan25@gmail.com>
2023-02-20 19:24:42 +0530

    learning functions and some problems

--- Guvi-daily-learning-practise/script.js
+++ Guvi-daily-learning-practise/script.js
@@ -33,6 +33,22 @@ request.onload=function() {
 
 // functions
 
+// 1.Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array.
+
+// Input:(foo([7, 9, 0, -2]))
+// output:7
+
+// Input:(foo([0,6,4,5]))
+// output:0
+
+// Input:(foo([1,2,3,4],-3))
+// Output:[];
+
+// Input:(foo([1,2,3,4],3))
+// Output:[1,2,3]
+
+// Input:(foo([1,2,3,4],5))
+// Output:[1,2,3,4]
 function foo(n,m){
     
          if(m==undefined){
@@ -51,7 +67,15 @@ function foo(n,m){
 //   console.log(foo([1,2,3,4],3));
 //   console.log(foo([1,2,3,4],5));
 // ----------------------------------------------------------------------------------------------------------------------------------------
-  function last(n,m){
+  
+// 2.Write a JavaScript function to get the last element of an array. Passing a parameter 'n' will return the last 'n' elements of the array.
+// Input :                                     	Expected Output :
+// console.log(last([7, 9, 0, -2]));         -2
+// console.log(last([7, 9, 0, -2],3));    [9, 0, -2]
+// console.log(last([7, 9, 0, -2],6));    [7, 9, 0, -2] 
+
+
+function last(n,m){
  
     if(m==undefined){
       return n[n.length-1] ;
@@ -65,3 +89,49 @@ function foo(n,m){
 // console.log(last([7, 9, 0, -2]));  -2       
 // console.log(last([7, 9, 0, -2],3));  [9,0,-2]  
 // console.log(last([7, 9, 0, -2],6));  [7,9,0,-2]
+
+// -----------------------------------------------------------------------------------------------------------------------------------
+
+// 3.Write a JavaScript function to generate an array between two integers of 1 step length.
+
+// Input :
+// console.log(range(3, 7));
+// Expected Output:
+// [3,4, 5, 6, 7]
+
+// Input :
+// console.log(range(-4, 7));
+// Expected Output:
+// [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
+
+function range(n,m){
+    var box=[];
+    for(var i=n;i<=m;i++){
+      box.push(i);
+       
+    }
+   return box;
+   }
+
+//    console.log(range(3, 7));
+//    console.log(range(-4, 7));
+
+// -----------------------------------------------------------------------------------------------------------------------------------
+
+
+// 4. Write a JavaScript function to remove a specific element from an array. 
+// Input :
+// console.log(remove([2, 5, 9, 6], 5));
+// Expected Output:
+// [2, 9, 6]
+
+function remove (a,b){
+    var box=[];
+  for(var i=0;i<a.length;i++){
+    if(b!==a[i]){
+     box.push(a[i]);     /// return a.splice(i,1);
+    }
+  }
+  return box;
+}
+// console.log(remove([2, 5, 9, 6], 5));
