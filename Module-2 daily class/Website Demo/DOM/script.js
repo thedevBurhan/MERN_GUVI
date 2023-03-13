14a5e0b9fb24d7960c954255214108d6db665456
thedevBurhan <smdburhan25@gmail.com>
2023-03-13 12:21:00 +0530

    Practise and learing

---
+++ Module-2 daily class/Website Demo/DOM/script.js
@@ -0,0 +1,12 @@
+function foo(){
+    var first=document.getElementById("firstname").value;
+    console.log(first);
+    var middle=document.getElementById("middlename").value;
+    console.log(middle);
+    var last=document.getElementById("lastname").value;
+   console.log(last);
+    var email=document.getElementById("email").value;
+   console.log(email);
+    var Password=document.getElementById("Password").value;
+    console.log(Password);
+    };
--- Module-2 daily class/script.js
+++
@@ -1,3 +0,0 @@
-var createSpan=document.createElement("span");
-createSpan.innerHTML="I am span";
-document.body.append(createSpan);
