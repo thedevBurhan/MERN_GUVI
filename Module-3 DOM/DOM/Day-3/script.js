0c7d35ae820da397715aaa869a553486fe34c193
thedevBurhan <smdburhan25@gmail.com>
2023-03-16 17:54:29 +0530

    task

--- Module-3 DOM/DOM/Day-3/script.js
+++ Module-3 DOM/DOM/Day-3/script.js
@@ -1,9 +1,53 @@
-var button=document.createElement("button");
-button.innerHTML="click Me";
-button.addEventListener("click",foo);
-document.body.append(button);
+var input=document.createElement('input');
+input.setAttribute("type","date");
+input.setAttribute("id","dob");
+input.setAttribute("class","main");
 
-function foo(){
-    var res=prompt("enter the age");
-    document.getElementById("demo").innerHTML = res;
+var button=document.createElement("button");
+button.innerHTML="Display date";
+button.setAttribute("class","btn btn-primary ml-2");
+button.addEventListener("click",displaydate);
+document.body.append(input,button);
+
+function displaydate(){
+    var inputs=document.getElementById("dob").value;
+    if(Date.parse(inputs)){
+        var inputdata=new Date(inputs);
+        document.getElementById("heading").innerHTML="Given input is"+" "+inputdata;
+        var currentdate=new Date();
+        var Millisecondsdiff=currentdate.getTime()-inputdata.getTime();
+        document.getElementById("milliseconds").innerHTML="Milli second"+" "+Millisecondsdiff;
+        var secondsdiff=Mathfloor(Millisecondsdiff,1000);
+        document.getElementById("second").innerHTML="Second"+" "+secondsdiff;
+        var minutediff=Mathfloor(secondsdiff,60);
+        document.getElementById("minute").innerHTML="Minute"+" "+minutediff;
+        var hourdiff=Mathfloor(minutediff,60);
+        document.getElementById("hour").innerHTML="Hour"+" "+hourdiff;
+
+        var daydiff=Mathfloor(hourdiff,24);
+        document.getElementById("day").innerHTML="Day"+" "+daydiff;
+
+        var yearsdiff=getyears(inputdata,currentdate);
+        document.getElementById("year").innerHTML="Year"+" "+yearsdiff;
+
+        var monthdiff=getmonths(inputdata,currentdate);
+        document.getElementById("month").innerHTML="Month"+" "+monthdiff;
+
+    }else{
+        console.log("invalid input entered");
+    }
+   
+}
+function Mathfloor(value1,value2){
+    return Math.floor(value1/value2);
+}
+function getyears(value1,value2){
+    var d1=new Date (value1);
+    var d2=new Date(value2);
+    return d2.getFullYear()-d1.getFullYear();
+}
+function getmonths(value1,value2){
+    var year=getyears(value1,value2);
+    var months=(year*12)+(value2.getMonth()-value1.getMonth());
+    return months;
 }
--- Module-3 DOM/Form-Task/script.js
+++ Module-3 DOM/Form-Task/script.js
@@ -1,6 +1,71 @@
-const form = document.getElementById("form");
-const fname = document.getElementById("fname");
-const food = document.getElementsByName("food");
+let submit = document.getElementById("submit");
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  // first name check----------------------------------------------------
+
+  if (fname.value == "") {
+    showError(fname, "First name is required");
+  } else if (fname.value.length < 3) {
+    showError(fname, "First name must be at least 3 character.");
+  } else {
+    showSuccess(fname);
+  }
+  //  -------------------------------------------------------------------
+
+  // address--------------------------------------------------------------
+  let address = [];
+  address.push(Address1.value);
+  address.push(Address2.value);
+  //  -------------------------------------------------------------------
+
+  let gender = document.getElementsByName("radio");
+  let genderValue = [];
+  for (let i = 0; i < gender.length; i++) {
+    if (gender[i].checked) {
+      genderValue.push(gender[i].value);
+    }
+  }
+  //  -------------------------------------------------------------------
+
+  // food ------------------------------------------------------------------
+
+  let food = document.getElementsByName("food");
+  let foodList = [];
+  let foodCount = 0;
+  console.log(food);
+  for (let i = 0; i < food.length; i++) {
+    if (food[i].checked) {
+      foodList.push(food[i].value);
+      foodCount++;
+    }
+  }
+  if (foodList.length >= 2) {
+    result = foodList.join(", ");
+  } else {
+    result = alert("Choose atleast 2 options out of 5 in food");
+  }
+  //  -------------------------------------------------------------------
+  createTable(
+    fname.value,
+    lname.value,
+    address.join(" "),
+    pincode.value,
+    genderValue.join(""),
+    result,
+    state.value,
+    country.value
+  );
+  fname.value = "";
+  lname.value = "";
+  Address1.value = "";
+  Address2.value = "";
+  pincode.value = "";
+  gender.value = "";
+  foodList = [];
+  state.value = "";
+  country.value = "";
+});
+
 // first name check---------------------------------------------------------
 
 // show input error
@@ -17,39 +82,54 @@ function showSuccess(input) {
   formControl.className = "firstname success";
 }
 // ---------------------------------------------------------------------------
+let fname = document.getElementById("fname");
+let lname = document.getElementById("lname");
+let Address1 = document.getElementById("Address1");
+let Address2 = document.getElementById("Address2");
+let pincode = document.getElementById("pincode");
+let gender = document.getElementById("gender");
+let food = document.getElementById("food");
+let state = document.getElementById("state");
+let country = document.getElementById("country");
 
-// Check box
-
-
-// event Listeners
-form.addEventListener("submit", function (a) {
-  a.preventDefault();
-  // The preventDefault() method of the Event interface
-  //  tells the user agent that if the event does not get explicitly handled,
-  // its default action should not be taken as it normally would be.
-  // first name check---------------------------------------------------------
-  if (fname.value == "") {
-    showError(fname, "First name is required");
-  } else if (fname.value.length < 3) {
-    showError(fname, "First name must be at least 3 character.");
-  } else {
-    showSuccess(fname);
-  }
-  // ---------------------------------------------------------------------------
-
-  // checkbox
-  let foodList = [];
-  let foodCount = 0;
-  
-  for (let i = 0; i < food.length; i++) {
-    if (food[i].checked) {
-      foodList.push(food[i].value);
-      foodCount++;
-    }
-  }
-  if (foodList.length >= 2) {
-    result = foodList.join(",");
-  } else {
-    result = alert("Choose atleast 2 options out of 5 in food");
-  }
-});
+function createTable(
+  fname,
+  lname,
+  address,
+  pincode,
+  gender,
+  food,
+  state,
+  country
+) {
+  let tbody = document.getElementById("tbody");
+  let tr = document.createElement("tr");
+  let td1 = document.createElement("td");
+  let td2 = document.createElement("td");
+  let td3 = document.createElement("td");
+  let td4 = document.createElement("td");
+  let td5 = document.createElement("td");
+  let td6 = document.createElement("td");
+  let td7 = document.createElement("td");
+  let td8 = document.createElement("td");
+  td1.innerHTML = fname;
+  td2.innerHTML = lname;
+  td3.innerHTML = address;
+  td4.innerHTML = pincode;
+  td5.innerHTML = gender;
+  td6.innerHTML = food;
+  td7.innerHTML = state;
+  td8.innerHTML = country;
+  tr.append(td1);
+  tr.append(td2);
+  tr.append(td3);
+  tr.append(td4);
+  tr.append(td5);
+  tr.append(td6);
+  tr.append(td7);
+  tr.append(td8);
+  tbody.append(tr);
+}
+function myFunction() {
+  document.getElementById("myForm").reset();
+}
