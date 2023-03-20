3e2438a70654141ecda8d254f2a2a10e898ddc26
thedevBurhan <smdburhan25@gmail.com>
2023-03-20 16:51:47 +0530

    chnages in code session task

--- Module-3 DOM/DOM/Day-3/script.js
+++ Module-3 DOM/DOM/Day-3/script.js
@@ -1,53 +1,79 @@
-var input = document.createElement("input");
-input.setAttribute("type", "date");
-input.setAttribute("id", "dob");
-input.setAttribute("class", "main");
+var input=document.createElement("input");
+input.setAttribute("type","date");
+input.setAttribute("id","dob");
 
-var button = document.createElement("button");
-button.innerHTML = "Display date";
-button.setAttribute("class", "btn btn-primary ml-2");
-button.addEventListener("click", displaydate);
-document.body.append(input, button);
+var button=document.createElement("button");
+button.innerHTML="Display data";
+button.setAttribute("class","btn btn-primary");
+button.addEventListener("click",displaydata);
 
-function displaydate() {
-  var inputs = document.getElementById("dob").value;
-  if (Date.parse(inputs)) {
-    var inputdata = new Date(inputs);
-    document.getElementById("heading").innerHTML =
-      "Given input is" + " " + inputdata;
-    var currentdate = new Date();
-    var Millisecondsdiff = currentdate.getTime() - inputdata.getTime();
-    document.getElementById("milliseconds").innerHTML =
-      "Milli second" + " " + Millisecondsdiff;
-    var secondsdiff = Mathfloor(Millisecondsdiff, 1000);
-    document.getElementById("second").innerHTML = "Second" + " " + secondsdiff;
-    var minutediff = Mathfloor(secondsdiff, 60);
-    document.getElementById("minute").innerHTML = "Minute" + " " + minutediff;
-    var hourdiff = Mathfloor(minutediff, 60);
-    document.getElementById("hour").innerHTML = "Hour" + " " + hourdiff;
+var div=document.createElement("div");
+div.setAttribute("id","display");
 
-    var daydiff = Mathfloor(hourdiff, 24);
-    document.getElementById("day").innerHTML = "Day" + " " + daydiff;
 
-    var yearsdiff = getyears(inputdata, currentdate);
-    document.getElementById("year").innerHTML = "Year" + " " + yearsdiff;
+function displaydata(){
+var inputs=document.getElementById("dob").value;
+console.log(typeof(inputs));
+if(Date.parse(inputs)){
+var inputdate=new Date(inputs);
+console.log(inputdate);
+var currentdate=new Date();
+console.log(currentdate);
+//Milliseconds diff
+var millisecdiff=currentdate.getTime()-inputdate.getTime();
+console.log(millisecdiff);
+//Seconds Diff
+var secondsdiff=mathfloor(millisecdiff,1000);
+console.log(secondsdiff);
+//Minutes diff
+var minutesdiff=mathfloor(secondsdiff,60);
+console.log(minutesdiff);
+//Hours diff
+var hoursdiff=mathfloor(minutesdiff,60);
+console.log(hoursdiff);
+//day diff
+var daydiff=mathfloor(hoursdiff,24);
+console.log(daydiff);
+//year diff
+var yeardiff=getyears(inputdate,currentdate);
+console.log(yeardiff);
+//months diff
+var monthdiff=getmonths(inputdate,currentdate);
+console.log(monthdiff);
+div.innerHTML=`
+Given Input date is ${inputs}<br>
+Year ${yeardiff}<br>
+months ${monthdiff}<br>
+days ${daydiff} <br>
+hour ${hoursdiff} <br>
+minute ${minutesdiff} <br>
+second ${secondsdiff} <br>
+milli sec ${millisecdiff}<br>
+`;
 
-    var monthdiff = getmonths(inputdata, currentdate);
-    document.getElementById("month").innerHTML = "Month" + " " + monthdiff;
-  } else {
+
+}
+else{
     console.log("invalid input entered");
-  }
 }
-function Mathfloor(value1, value2) {
-  return Math.floor(value1 / value2);
 }
-function getyears(value1, value2) {
-  var d1 = new Date(value1);
-  var d2 = new Date(value2);
-  return d2.getFullYear() - d1.getFullYear();
+
+
+function mathfloor(value1,value2){
+return Math.floor(value1/value2);
 }
-function getmonths(value1, value2) {
-  var year = getyears(value1, value2);
-  var months = year * 12 + (value2.getMonth() - value1.getMonth());
-  return months;
+
+function getyears(value1,value2){
+var d1=new Date(value1);
+var d2=new Date(value2);
+return d2.getFullYear()-d1.getFullYear();
 }
+
+
+function getmonths(value1,value2){
+    var year=getyears(value1,value2);
+    var months=(year*12)+(value2.getMonth()-value1.getMonth());
+    return months;
+}
+
+document.body.append(input,button,div);
