0a1dce0f2766b0a4a8e831a54b91b726aaac1d45
thedevBurhan <smdburhan25@gmail.com>
2023-03-24 17:00:38 +0530

    changes in task code

--- Module-4/Rest countries task/script.js
+++ Module-4/Rest countries task/script.js
@@ -4,10 +4,14 @@ var row = document.createElement("div");
 row.classList.add("row", "m-4");
 container.append(row);
 
+var span=document.createElement("span");
+row.append(span);
+
+
 var res = fetch("https://restcountries.com/v2/all"); //this is return Promise in readable stream(A packets od data);
 res
   .then((data) => data.json()) //this will return promise in object
-  .then((data1) => foo(data1) ) //this will return array of object
+  .then((data1) => console.log(data1) ) //this will return array of object
   .catch((error) => console.log(error));
 
 function foo(data1) {
@@ -34,7 +38,7 @@ function foo(data1) {
                 
                   <p class="card-text mb-3">Country Code: ${data1[i].alpha3Code}</p>
                   
-                   <button type="button" class="btn btn-secondary"><a href="https://openweathermap.org/>Click for Weather</button>
+                   <button type="button" class="btn btn-secondary" onclick=getdata();>Click for Weather</button>
                </div>
            </div>
         
@@ -42,3 +46,33 @@ function foo(data1) {
   }
   document.body.append(container);
 }
+
+//Multiple api with async and await
+async function getdata(){
+  var res=await fetch("https://restcountries.com/v2/all");
+  var res1= await res.json();
+
+  for(var i=0;i<res1.length;i++){
+      try {
+          // console.log(`Latitude:${res1[i].latlng[0]} Longitude:${res1[i].latlng[1]}`);
+          weatherdata(res1[i].latlng[0],res1[i].latlng[1]);
+      } 
+ catch (error) {
+  console.log(error);
+}
+}
+}
+
+async function weatherdata(lat,lon){
+try {
+  if(lon===undefined) throw new Error("Invalid Coordinates");
+  // console.log(lat,lon);
+  let res2=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=fa4973b1fa1ca717811b9566c55321ec`);
+  let res3=await res2.json();
+  console.log(`${res3.main.temp}`);
+} catch (error) {
+ console.log(error) 
+}
+
+}
+// getdata();
