afb1607d8e40b462fd56a179ecd40c3822fa3bb7
thedevBurhan <smdburhan25@gmail.com>
2023-04-02 17:18:55 +0530

    youtube clone

---
+++ youtube/script.js
@@ -0,0 +1,8 @@
+// var Container=document.getElementsByClassName('video-container');
+// console.log(Container);
+// for(var i=0;i<=100;i++){
+//     var Container=document.getElementsByClassName('video-container');
+// console.log(Container);
+//     document.body.append(Container);
+// }
+
